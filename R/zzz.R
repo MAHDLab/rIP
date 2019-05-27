d1aadee626cc6aa97d073d6125beffb91ae44284
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2019-05-27 10:50:32 -0600

    onattach cite notification

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Please cite use of rIP: Waggoner, Philip D., Ryan Kennedy, and Scott Clifford, (2019). Detecting Fraud in Online Surveys by Tracing, Scoring, and Visualizing IP Addresses. Journal of Open Source Software, 4(37), 1285, https://doi.org/10.21105/joss.01285")
+}
